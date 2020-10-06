5e07f2e222df75ca432857e9df5a8c565742c878
nathanmailhiot <nathanjm12@outlook.com>
2020-10-06 11:49:04 -0400

    Deploy prep

---
+++ frontend/build/precache-manifest.131cfb7eb02037a5439e5bf25b5e1e23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc09d87a2b3302bdf8d17689e7a0bad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8583155e5d808b612d58",
+    "url": "/static/css/main.66e1e31a.chunk.css"
+  },
+  {
+    "revision": "1b9f064752d52149047c",
+    "url": "/static/js/2.06076290.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.06076290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8583155e5d808b612d58",
+    "url": "/static/js/main.27597564.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
